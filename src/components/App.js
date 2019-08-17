af5293eda96b60d30954512d9a2548273e8aa49c
souptik5 <36810831+souptik5@users.noreply.github.com>
2019-08-17 20:27:03 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>React App</div>;
+  }
+}
+
+// const AppWithHeader = () => {
+//   return <Header Component={App} />;
+// };
+
+export default App;
